33bfa59706fb53161cba62aeb58764bd6b06d214
SuperNova911 <suwhan77@naver.com>
2015-11-11 00:52:17 +0900

    AutoLevelUp, DrawWatermark Updated
    Todo - AutoLevelUp Custom Logic

--- Essential/Essential/Program.cs
+++ Essential/Essential/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Essential.AutoLevel;
+using Essential.Hud;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+/*
+ *          만들어야 할 것
+ *      리콜 트래커
+ *      와드 트래커
+ *      자동 레벨업 - 커스텀 로직 만들기
+ *      아이템 사용
+ *      
+ *          만든거
+ *      워터마크
+ *      자동 레벨업
+ */
 
 namespace Essential
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static Menu Menu;
+        private static void Main(string[] args)
         {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+
+            
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Menu = MainMenu.AddMenu("Essential", "ess");
+            AutoLevelUp.Init();
+            HudDraw.Init();
+
+            if (HudDraw.watermark())
+            {
+                EloBuddy.Hacks.RenderWatermark = true;
+            }
+            else
+            {
+                EloBuddy.Hacks.RenderWatermark = false;
+            }
         }
     }
 }
--- Essential/Essential/Properties/AssemblyInfo.cs
+++ Essential/Essential/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Essential")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © SuperNova911 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 버전이 자동으로
 // 지정되도록 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
