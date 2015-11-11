9b1cb646d3f2eea59a5651fc08bf62f7e1e20a7f
SuperNova911 <suwhan77@naver.com>
2015-11-11 19:50:27 +0900

    GankAlert base made

---
+++ Essential/Essential/Draw/GankAlert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using SharpDX;
+
+namespace Essential.Draw
+{
+    internal class GankAlert
+    {
+        public static Menu GankMenu;
+
+        public static void Init()
+        {
+            var menu = Program.Menu;
+
+            GankMenu = menu.AddSubMenu("GankAlert");
+
+            GankMenu.AddGroupLabel("Ally");
+            GankMenu.Add("allySmite", new CheckBox("Warn Jungler (Smite)"));
+            GankMenu.AddGroupLabel("Enemy");
+        }
+    }
+}
