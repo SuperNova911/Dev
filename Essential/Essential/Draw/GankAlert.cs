97bdeb90f4ae060a9cdfed4d3aa0b54ff5a31bbd
SuperNova911 <suwhan77@naver.com>
2015-11-11 23:20:34 +0900

    added some codes

--- Essential/Essential/Draw/GankAlert.cs
+++ Essential/Essential/Draw/GankAlert.cs
@@ -7,6 +7,7 @@ using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Menu;
 using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
 using SharpDX;
 
 namespace Essential.Draw
@@ -14,6 +15,7 @@ namespace Essential.Draw
     internal class GankAlert
     {
         public static Menu GankMenu;
+        private readonly IDictionary<int, Champion> _championInfoById = new Dictionary<int, Champion>();
 
         public static void Init()
         {
@@ -21,9 +23,54 @@ namespace Essential.Draw
 
             GankMenu = menu.AddSubMenu("GankAlert");
 
-            GankMenu.AddGroupLabel("Ally");
-            GankMenu.Add("allySmite", new CheckBox("Warn Jungler (Smite)"));
             GankMenu.AddGroupLabel("Enemy");
+            foreach (var enemy in EntityManager.Heroes.Enemies)
+            {
+                GankMenu.Add(enemy.ChampionName, new CheckBox(enemy.ChampionName));
+            }
+            GankMenu.Add("ememySmite", new CheckBox("Warn Jungler Only (Smite)"));
+
+            GankMenu.AddGroupLabel("Ally");
+            foreach (var ally in EntityManager.Heroes.Allies)
+            {
+                GankMenu.Add(ally.ChampionName, new CheckBox(ally.ChampionName));
+            }
+            GankMenu.Add("allySmite", new CheckBox("Warn Jungler Only (Smite)"));
+
+            GankMenu.AddGroupLabel("Draw");
+            GankMenu.Add("range", new Slider("Alert Range", 3000, 500, 5000));
+            GankMenu.Add("cooldown", new Slider("Trigger Cooldown", 10, 0, 60));
+            GankMenu.Add("duration", new Slider("Line Duration", 10, 0, 20));
+            GankMenu.Add("champName", new CheckBox("Show Champion Name", true));
+            GankMenu.Add("minimapLine", new CheckBox("Draw on Minimap", false));
+            GankMenu.Add("ping", new CheckBox("Danger Ping (Local)", false));
+        }
+    }
+
+    internal class Champion
+    {
+        private static int index = 0;
+
+        private readonly AIHeroClient hero;
+        private readonly bool ally;
+        private int textOffSet;
+
+        private event EventHandler OnRange;
+
+        private bool visable;
+        private float distance;
+        private float lastEnter;
+        private float lineStart;
+        private int lineWidth;
+
+        public Champion(AIHeroClient _hero, bool _ally)
+        {
+            index++;
+            textOffSet = index * 50;
+            hero = _hero;
+            ally = _ally;
+
+            
         }
     }
 }
