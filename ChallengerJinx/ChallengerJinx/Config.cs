afef23e649ce42f97c15d13e5856257b564c3006
SuperNova911 <suwhan77@naver.com>
2015-11-18 17:20:10 +0900

    add more

--- ChallengerJinx/ChallengerJinx/Config.cs
+++ ChallengerJinx/ChallengerJinx/Config.cs
@@ -53,7 +53,127 @@ namespace ChallengerJinx
             {
             }
 
+            public static class Combo
+            {
+                private static readonly CheckBox _useQ;
+                private static readonly CheckBox _useW;
+                private static readonly CheckBox _useE;
+                private static readonly CheckBox _useR;
 
+                public static bool UseQ { get { return _useQ.CurrentValue; } }
+                public static bool UseW { get { return _useW.CurrentValue; } }
+                public static bool UseE { get { return _useE.CurrentValue; } }
+                public static bool UseR { get { return _useR.CurrentValue; } }
+
+                static Combo()
+                {
+                    Menu.AddGroupLabel("Combo");
+
+                    _useQ = Menu.Add("comboUseQ", new CheckBox("Use Q"));
+                    _useW = Menu.Add("comboUseW", new CheckBox("Use W"));
+                    _useE = Menu.Add("comboUseE", new CheckBox("Use E"));
+                    _useR = Menu.Add("comboUseR", new CheckBox("Use R"));
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+
+            public static class Harass
+            {
+                private static readonly CheckBox _useQ;
+                private static readonly CheckBox _useW;
+                private static readonly Slider _mana;
+
+                public static bool UseQ { get { return _useQ.CurrentValue; } }
+                public static bool UseW { get { return _useW.CurrentValue; } }
+                public static int Mana { get { return _mana.CurrentValue; } }
+
+                static Harass()
+                {
+                    Menu.AddGroupLabel("Harass");
+
+                    _useQ = Menu.Add("harassUseQ", new CheckBox("Use Q"));
+                    _useW = Menu.Add("harassUseW", new CheckBox("Use W"));
+                    _mana = Menu.Add("harassMana", new Slider("Minimum mana in %", 30));
+                }
+
+                public static Initialize()
+                {
+                }
+            }
+
+            public static class LaneClear
+            {
+                private static readonly CheckBox _useQ;
+                private static readonly Slider _mana;
+
+                public static bool UseQ { get { return _useQ.CurrentValue; } }
+                public static int Mana { get { return _mana.CurrentValue; } }
+
+                static LaneClear()
+                {
+                    Menu.AddGroupLabel("LaneClear");
+
+                    _useQ = Menu.Add("laneUseQ", new CheckBox("Use Q"));
+                    _mana = Menu.Add("laneMana", new Slider("Minimum mana in %", 30));
+                }
+
+                public static Initialize()
+                {
+                }
+            }
+
+            public static class JungleClear
+            {
+                private static readonly CheckBox _useQ;
+                private static readonly CheckBox _useW;
+                private static readonly Slider _mana;
+
+                public static bool UseQ { get { return _useQ.CurrentValue; } }
+                public static bool UseW { get { return _useW.CurrentValue; } }
+                public static int Mana { get { return _mana.CurrentValue; } }
+
+                static JungleClear()
+                {
+                    Menu.AddGroupLabel("JungleClear");
+
+                    _useQ = Menu.Add("jungleUseQ", new CheckBox("Use Q"));
+                    _useW = Menu.Add("jungleUseW", new CheckBox("Use W"));
+                    _mana = Menu.Add("jungleMana", new Slider("Minimum mana in %", 30));
+                }
+
+                public static Initialize()
+                {
+                }
+            }
+
+            public static class Flee
+            {
+                private static readonly CheckBox _useW;
+                private static readonly CheckBox _useE;
+                
+                public static bool UseW { get { return _useW.CurrentValue; } }
+                public static bool UseE { get { return _useE.CurrentValue; } }
+
+                static Flee()
+                {
+                    Menu.AddGroupLabel("Flee");
+
+                    _useW = Menu.Add("fleeUseW", new CheckBox("Use W"));
+                    _useE = Menu.Add("fleeUseE", new CheckBox("Use E"));
+                }
+
+                public static Initialize()
+                {
+                }
+            }
+        }
+
+        public static class Drawing
+        {
+            private static Menu Menu 
         }
     }
 }
--- ChallengerJinx/ChallengerJinx/Jinx.cs
+++ ChallengerJinx/ChallengerJinx/Jinx.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChallengerJinx
 {
-    class Program
+    class Jinx
     {
         static void Main(string[] args)
         {
