fb5fe9d55adb5fc44f41bf9ead6e4a0aa39a3a0f
SuperNova911 <suwhan77@naver.com>
2015-11-29 15:36:09 +0900

    Todo

---
+++ ChallengerBlitzcrank/ChallengerBlitzcrank/Todo.cs
@@ -0,0 +1,48 @@
+﻿/*
+	Q
+Combo
+Harass
+Interrupt
+Killsteal
+Immobile
+Dash
+Hitchance
+Min/Max Range
+Mana
+Health
+Search Range
+
+	E
+Combo
+Harass
+Interrupt
+Killsteal
+Immobile
+Mana
+Auto Use
+AA Cancel
+
+	R
+Combo
+Harass
+Interrupt (preferred over Q)
+Killsteal
+Immobile
+Gapclose
+Mana
+Auto R
+
+	Mode
+Grab Mode
+Q E R
+R Q E
+Smite Q
+Flash Q
+
+	Draw
+Q
+R
+Q Target
+Target Hitchance
+Damage Indicator
+*/
