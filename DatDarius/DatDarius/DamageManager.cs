9503e4a8a5822a8c3034824a2e9bd551a4e3f2e0
SuperNova911 <suwhan77@naver.com>
2016-01-13 00:23:56 +0900

    Rewrite

---
+++ DatDarius/DatDarius/DamageManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+
+namespace DatDarius
+{
+    public static class DamageManager
+    {
+        public static double RDamage(this AIHeroClient target)
+        {
+            return (new double[] { 100, 200, 300 }[SpellManager.R.Level - 1] + 0.75 * Player.Instance.FlatPhysicalDamageMod) * (1 + 0.2 * target.BuffCount("dariushemo"));
+        }
+
+        public static double RDamage(this AIHeroClient target, int stack)
+        {
+            return (new double[] { 100, 200, 300 }[SpellManager.R.Level - 1] + 0.75 * Player.Instance.FlatPhysicalDamageMod) * (1 + 0.2 * stack);
+        }
+
+        public static int IgniteDamage(int second = 5)
+        {
+            return (10 + 4 * Player.Instance.Level) * second;
+        }
+
+        public static float PassiveDamage(this AIHeroClient target)
+        {
+            float damagePerSec = (9 + Player.Instance.Level + 0.3f * Player.Instance.FlatPhysicalDamageMod) / 5;
+
+            return Damage.CalculateDamageOnUnit(Player.Instance, target, DamageType.Physical, damagePerSec * target.BuffCount("dariushemo") * target.BuffRemainTime("dariushemo"), false, false);
+        }
+
+        public static float PassiveDamage(this AIHeroClient target, int stack, int second)
+        {
+            float damage = ((9 + Player.Instance.Level + 0.3f * Player.Instance.FlatPhysicalDamageMod) / 5) * stack * second;
+
+            return Damage.CalculateDamageOnUnit(Player.Instance, target, DamageType.Physical, damage, false, false);
+        }
+    }
+}
--- DatDarius/DatDarius/HealHandler.cs
+++ DatDarius/DatDarius/HealHandler.cs
@@ -3,121 +3,118 @@ using EloBuddy.SDK;
 
 namespace DatDarius
 {
-    public class HealHandler
+    public static class HealHandler
     {
-        public class Potion
+        /// <summary>
+        /// 대상이 포션이 있는지 확인.
+        /// </summary>
+        /// <param name="unit">대상.</param>
+        /// <returns><c>0</c> 포션 없음, <c>1</c> Health Portion, <c>2</c> Biscuit, <c>3</c> Refillable Portion, <c>4</c> Hunter's Portion, <c>5</c> Corrupting Portion.</returns>
+        public static int HasPotion(this AIHeroClient unit)
         {
-            /// <summary>
-            /// 대상이 포션이 있는지 확인.
-            /// </summary>
-            /// <param name="unit">대상.</param>
-            /// <returns><c>0</c> 포션 없음, <c>1</c> Health Portion, <c>2</c> Biscuit, <c>3</c> Refillable Portion, <c>4</c> Hunter's Portion, <c>5</c> Corrupting Portion.</returns>
-            public static int HasPotion(AIHeroClient unit)
-            {
-                if (Item.HasItem(ItemId.Health_Potion, unit))
-                    return 1;
-                else if (Item.HasItem(ItemId.Rejuvenation_Bead, unit))
-                    return 2;
-                else if (Item.HasItem(2031, unit))
-                    return 3;
-                else if (Item.HasItem(2032, unit))
-                    return 4;
-                else if (Item.HasItem(2033, unit))
-                    return 5;
-                else
-                    return 0;
-            }
-
-            /// <summary>
-            /// 대상이 포션으로 체력을 회복하고 있는지 확인.
-            /// </summary>
-            /// <param name="unit">대상.</param>
-            /// <returns>true == 회복 중.</returns>
-            public static bool IsHealing(AIHeroClient unit)
-            {
-                if (unit.HasBuff("RegenerationPotion") ||
-                    unit.HasBuff("ItemMiniRegenPotion") ||
-                    unit.HasBuff("ItemCrystalFlask") ||
-                    unit.HasBuff("ItemCrystalFlaskJungle") ||
-                    unit.HasBuff("ItemDarkCrystalFlask"))
-                    return true;
-                else
-                    return false;
-            }
-
-            /// <summary>
-            /// 포션을 가진 대상이 포션으로 초당 얼마의 체력을 회복할수 있는지 계산.
-            /// </summary>
-            /// <param name="unit">대상.</param>
-            /// <returns>초당 회복량.</returns>
-            public static double HealTick(AIHeroClient unit)
-            {
-                switch (HasPotion(unit))
-                {
-                    case 0:
-                        return 0;
-                    case 1:
-                        return unit.HasBuff("RegenerationPotion") ? 0 : 150 / 15;
-                    case 2:
-                        return unit.HasBuff("ItemMiniRegenPotion") ? 0 : (150 / 15) + 20;
-                    case 3:
-                        return unit.HasBuff("ItemCrystalFlask") ? 0 : 125 / 12;
-                    case 4:
-                        return unit.HasBuff("ItemCrystalFlaskJungle") ? 0 : 60 / 8;
-                    case 5:
-                        return unit.HasBuff("ItemDarkCrystalFlask") ? 0 : 150 / 12;
-                }
+            if (Item.HasItem(ItemId.Health_Potion, unit))
+                return 1;
+            else if (Item.HasItem(ItemId.Rejuvenation_Bead, unit))
+                return 2;
+            else if (Item.HasItem(2031, unit))
+                return 3;
+            else if (Item.HasItem(2032, unit))
+                return 4;
+            else if (Item.HasItem(2033, unit))
+                return 5;
+            else
                 return 0;
-            }
+        }
 
-            /// <summary>
-            /// 대상이 몇 초간 얼마나 체력을 회복할지 계산.
-            /// </summary>
-            /// <param name="unit">대상.</param>
-            /// <param name="second">시간(s).</param>
-            /// <returns>몇 초간 얻는 회복량.</returns>
-            public static double GetHeal(AIHeroClient unit, int second)
+        /// <summary>
+        /// 대상이 포션으로 체력을 회복하고 있는지 확인.
+        /// </summary>
+        /// <param name="unit">대상.</param>
+        /// <returns>true == 회복 중.</returns>
+        public static bool IsHealing(this AIHeroClient unit)
+        {
+            if (unit.HasBuff("RegenerationPotion") ||
+                unit.HasBuff("ItemMiniRegenPotion") ||
+                unit.HasBuff("ItemCrystalFlask") ||
+                unit.HasBuff("ItemCrystalFlaskJungle") ||
+                unit.HasBuff("ItemDarkCrystalFlask"))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// 포션을 가진 대상이 포션으로 초당 얼마의 체력을 회복할수 있는지 계산.
+        /// </summary>
+        /// <param name="unit">대상.</param>
+        /// <returns>초당 회복량.</returns>
+        public static double HealTick(AIHeroClient unit)
+        {
+            switch (unit.HasPotion())
             {
-                if (unit.HasBuff("RegenerationPotion"))
-                {
-                    if (unit.GetBuff("RegenerationPotion").EndTime - Game.Time < second)
-                        return (150 / 15) * unit.GetBuff("RegenerationPotion").EndTime - Game.Time;
-                    else
-                        return (150 / 15) * second;
-                }
-                else if (unit.HasBuff("ItemMiniRegenPotion"))
-                {
-                    if (unit.GetBuff("ItemMiniRegenPotion").EndTime - Game.Time < second)
-                        return (150 / 15) * unit.GetBuff("ItemMiniRegenPotion").EndTime - Game.Time;
-                    else
-                        return (150 / 15) * second;
-                }
-                else if (unit.HasBuff("ItemCrystalFlask"))
-                {
-                    if (unit.GetBuff("ItemCrystalFlask").EndTime - Game.Time < second)
-                        return (125 / 12) * unit.GetBuff("ItemCrystalFlask").EndTime - Game.Time;
-                    else
-                        return (125 / 12) * second;
-                }
-                else if (unit.HasBuff("ItemCrystalFlaskJungle"))
-                {
-                    if (unit.GetBuff("ItemCrystalFlaskJungle").EndTime - Game.Time < second)
-                        return (60 / 8) * unit.GetBuff("ItemCrystalFlaskJungle").EndTime - Game.Time;
-                    else
-                        return (60 / 8) * second;
-
-                }
-                else if (unit.HasBuff("ItemDarkCrystalFlask"))
-                {
-                    if (unit.GetBuff("ItemDarkCrystalFlask").EndTime - Game.Time < second)
-                        return (150 / 12) * unit.GetBuff("ItemDarkCrystalFlask").EndTime - Game.Time;
-                    else
-                        return (150 / 12) * second;
-
-                }
-                else
+                case 0:
                     return 0;
+                case 1:
+                    return unit.HasBuff("RegenerationPotion") ? 0 : 150 / 15;
+                case 2:
+                    return unit.HasBuff("ItemMiniRegenPotion") ? 0 : (150 / 15) + 20;
+                case 3:
+                    return unit.HasBuff("ItemCrystalFlask") ? 0 : 125 / 12;
+                case 4:
+                    return unit.HasBuff("ItemCrystalFlaskJungle") ? 0 : 60 / 8;
+                case 5:
+                    return unit.HasBuff("ItemDarkCrystalFlask") ? 0 : 150 / 12;
             }
+            return 0;
+        }
+
+        /// <summary>
+        /// 대상이 몇 초간 얼마나 체력을 회복할지 계산.
+        /// </summary>
+        /// <param name="unit">대상.</param>
+        /// <param name="second">시간(s).</param>
+        /// <returns>몇 초간 얻는 회복량.</returns>
+        public static double GetHeal(AIHeroClient unit, int second)
+        {
+            if (unit.HasBuff("RegenerationPotion"))
+            {
+                if (unit.GetBuff("RegenerationPotion").EndTime - Game.Time < second)
+                    return (150 / 15) * unit.GetBuff("RegenerationPotion").EndTime - Game.Time;
+                else
+                    return (150 / 15) * second;
+            }
+            else if (unit.HasBuff("ItemMiniRegenPotion"))
+            {
+                if (unit.GetBuff("ItemMiniRegenPotion").EndTime - Game.Time < second)
+                    return (150 / 15) * unit.GetBuff("ItemMiniRegenPotion").EndTime - Game.Time;
+                else
+                    return (150 / 15) * second;
+            }
+            else if (unit.HasBuff("ItemCrystalFlask"))
+            {
+                if (unit.GetBuff("ItemCrystalFlask").EndTime - Game.Time < second)
+                    return (125 / 12) * unit.GetBuff("ItemCrystalFlask").EndTime - Game.Time;
+                else
+                    return (125 / 12) * second;
+            }
+            else if (unit.HasBuff("ItemCrystalFlaskJungle"))
+            {
+                if (unit.GetBuff("ItemCrystalFlaskJungle").EndTime - Game.Time < second)
+                    return (60 / 8) * unit.GetBuff("ItemCrystalFlaskJungle").EndTime - Game.Time;
+                else
+                    return (60 / 8) * second;
+
+            }
+            else if (unit.HasBuff("ItemDarkCrystalFlask"))
+            {
+                if (unit.GetBuff("ItemDarkCrystalFlask").EndTime - Game.Time < second)
+                    return (150 / 12) * unit.GetBuff("ItemDarkCrystalFlask").EndTime - Game.Time;
+                else
+                    return (150 / 12) * second;
+
+            }
+            else
+                return 0;
         }
     }
 }
