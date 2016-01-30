abe5c1c0c75067b1d2a4eb5cd7f1f4bf3e86bd16
SuperNova911 <suwhan77@naver.com>
2016-01-30 23:39:24 +0900

    New ColorMenu and more

--- TimerBuddy/TimerBuddy/ColorMenu.cs
+++ TimerBuddy/TimerBuddy/ColorMenu.cs
@@ -1,68 +1,69 @@
-﻿using SharpDX;
-using System.Linq;
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
 using EloBuddy.SDK.Menu;
 using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TimerBuddy
 {
     public static class ColorMenu
     {
+        static Font TestFont = new Font(Drawing.Direct3DDevice, new System.Drawing.Font("Gill Sans MT Pro", 30));
+
         private static readonly Color[] Colors =
         {
-            Color.AliceBlue, Color.LightSalmon, Color.AntiqueWhite, Color.LightSeaGreen, Color.Aqua, Color.LightSkyBlue,
-            Color.Aquamarine, Color.LightSlateGray, Color.Azure, Color.LightSteelBlue, Color.Beige,
-            Color.LightYellow, Color.Bisque, Color.Lime, Color.Black, Color.LimeGreen, Color.BlanchedAlmond, Color.Linen,
-            Color.Blue, Color.Magenta, Color.BlueViolet, Color.Maroon, Color.Brown, Color.MediumAquamarine,
-            Color.BurlyWood, Color.MediumBlue, Color.CadetBlue, Color.MediumOrchid, Color.Chartreuse, Color.MediumPurple,
-            Color.Chocolate, Color.MediumSeaGreen, Color.Coral, Color.MediumSlateBlue, Color.CornflowerBlue,
-            Color.MediumSpringGreen, Color.Cornsilk, Color.MediumTurquoise, Color.Crimson, Color.MediumVioletRed,
-            Color.Cyan, Color.MidnightBlue, Color.DarkBlue, Color.MintCream, Color.DarkCyan, Color.MistyRose,
-            Color.DarkGoldenrod, Color.Moccasin, Color.DarkGray, Color.NavajoWhite, Color.DarkGreen, Color.Navy,
-            Color.DarkKhaki, Color.OldLace, Color.DarkMagenta, Color.Olive, Color.DarkOliveGreen, Color.OliveDrab,
-            Color.DarkOrange, Color.Orange, Color.DarkOrchid, Color.OrangeRed, Color.DarkRed, Color.Orchid,
-            Color.DarkSalmon, Color.PaleGoldenrod, Color.DarkSeaGreen, Color.PaleGreen, Color.DarkSlateBlue,
-            Color.PaleTurquoise, Color.DarkSlateGray, Color.PaleVioletRed, Color.DarkTurquoise, Color.PapayaWhip,
-            Color.DarkViolet, Color.PeachPuff, Color.DeepPink, Color.Peru, Color.DeepSkyBlue, Color.Pink,
-            Color.DimGray, Color.Plum, Color.DodgerBlue, Color.PowderBlue, Color.Firebrick, Color.Purple,
-            Color.FloralWhite, Color.Red, Color.ForestGreen, Color.RosyBrown, Color.Fuchsia, Color.RoyalBlue,
-            Color.Gainsboro, Color.SaddleBrown, Color.GhostWhite, Color.Salmon, Color.Gold, Color.SandyBrown,
-            Color.Goldenrod, Color.SeaGreen, Color.Gray, Color.SeaShell, Color.Green, Color.Sienna, Color.GreenYellow,
-            Color.Silver, Color.Honeydew, Color.SkyBlue, Color.HotPink, Color.SlateBlue, Color.IndianRed,
-            Color.SlateGray, Color.Indigo, Color.Snow, Color.Ivory, Color.SpringGreen, Color.Khaki, Color.SteelBlue,
-            Color.Lavender, Color.Tan, Color.LavenderBlush, Color.Teal, Color.LawnGreen, Color.Thistle,
-            Color.LemonChiffon, Color.Tomato, Color.LightBlue, Color.Turquoise, Color.LightCoral, Color.Violet,
-            Color.LightCyan, Color.Wheat, Color.LightGoldenrodYellow, Color.White, Color.LightGreen, Color.WhiteSmoke,
-            Color.LightGray, Color.Yellow, Color.LightPink, Color.YellowGreen
+            Color.White, Color.WhiteSmoke, Color.Gainsboro, Color.LightGray, Color.DarkGray,
+            Color.DimGray, Color.Black, Color.DarkRed, Color.Firebrick, Color.Brown,
+            Color.Crimson, Color.Red, Color.OrangeRed, Color.DarkOrange, Color.Orange,
+            Color.Goldenrod, Color.Gold, Color.Yellow, Color.Olive, Color.YellowGreen,
+            Color.OliveDrab, Color.Green, Color.ForestGreen, Color.LimeGreen, Color.Lime,
+            Color.LawnGreen, Color.Chartreuse, Color.MediumSpringGreen, Color.SpringGreen, Color.Cyan,
+            Color.Aqua, Color.Aquamarine, Color.DeepSkyBlue, Color.SkyBlue, Color.LightSkyBlue,
+            Color.DodgerBlue, Color.RoyalBlue, Color.Blue, Color.MediumBlue, Color.DarkBlue,
+            Color.DarkSlateBlue, Color.MediumSlateBlue, Color.MediumPurple, Color.BlueViolet, Color.DarkOrchid,
+            Color.Purple, Color.Fuchsia, Color.Magenta, Color.DeepPink, Color.Violet,
+            Color.HotPink
         };
 
         private static readonly string[] ColorsName =
         {
-            "AliceBlue", "LightSalmon", "AntiqueWhite", "LightSeaGreen", "Aqua", "LightSkyBlue", "Aquamarine",
-            "LightSlateGray", "Azure", "LightSteelBlue", "Beige", "LightYellow", "Bisque", "Lime", "Black",
-            "LimeGreen", "BlanchedAlmond", "Linen", "Blue", "Magenta", "BlueViolet", "Maroon", "Brown",
-            "MediumAquamarine", "BurlyWood", "MediumBlue", "CadetBlue", "MediumOrchid", "Chartreuse",
-            "MediumPurple", "Chocolate", "MediumSeaGreen", "Coral", "MediumSlateBlue", "CornflowerBlue",
-            "MediumSpringGreen", "Cornsilk", "MediumTurquoise", "Crimson", "MediumVioletRed", "Cyan", "MidnightBlue",
-            "DarkBlue", "MintCream", "DarkCyan", "MistyRose", "DarkGoldenrod", "Moccasin", "DarkGray", "NavajoWhite",
-            "DarkGreen", "Navy", "DarkKhaki", "OldLace", "DarkMagena", "Olive", "DarkOliveGreen", "OliveDrab",
-            "DarkOrange", "Orange", "DarkOrchid", "OrangeRed", "DarkRed", "Orchid", "DarkSalmon", "PaleGoldenrod",
-            "DarkSeaGreen", "PaleGreen","DarkSlateBlue","PaleTurquoise", "DarkSlateGray", "PaleVioletRed",
-            "DarkTurquoise", "PapayaWhip", "DarkViolet", "PeachPuff", "DeepPink", "Peru", "DeepSkyBlue",
-            "Pink", "DimGray", "Plum", "DodgerBlue", "PowderBlue", "Firebrick","Purple", "FloralWhite", "Red",
-            "ForestGreen", "RosyBrown", "Fuschia", "RoyalBlue", "Gainsboro", "SaddleBrown", "GhostWhite", "Salmon",
-            "Gold", "SandyBrown", "Goldenrod", "SeaGreen", "Gray", "Seashell", "Green", "Sienna", "GreenYellow",
-            "Silver", "Honeydew", "SkyBlue", "HotPink", "SlateBlue", "IndianRed", "SlateGray", "Indigo", "Snow",
-            "Ivory", "SpringGreen", "Khaki", "SteelBlue", "Lavender", "Tan", "LavenderBlush", "Teal", "LawnGreen",
-            "Thistle", "LemonChiffon", "Tomato", "LightBlue", "Turquoise", "LightCoral", "Violet", "LightCyan",
-            "Wheat", "LightGoldenrodYellow", "White", "LightGreen", "WhiteSmoke", "LightGray", "Yellow", "LightPink",
-            "YellowGreen"
+            "White", "WhiteSmoke", "Gainsboro", "LightGray", "DarkGray", "DimGray", "Black", 
+            "DarkRed", "Firebrick", "Brown", "Crimson", "Red", "OrangeRed",
+            "DarkOrange", "Orange", "Goldenrod", "Gold", "Yellow", "Olive", "YellowGreen",
+            "OliveDrab", "Green", "ForestGreen", "LimeGreen", "Lime", "LawnGreen",
+            "Chartreuse", "MediumSpringGreen", "SpringGreen", "Cyan", "Aqua", "Aquamarine",
+            "DeepSkyBlue", "SkyBlue", "LightSkyBlue", "DodgerBlue", "RoyalBlue",
+            "Blue", "MediumBlue", "DarkBlue", "DarkSlateBlue", "MediumSlateBlue",
+            "MediumPurple", "BlueViolet", "DarkOrchid", "Purple", "Fuchsia", "Magenta",
+            "DeepPink", "Violet", "HotPink"
         };
 
         public static void AddColorItem(this Menu menu, string uniqueId, int defaultColour = 0)
         {
             var a = menu.Add(uniqueId, new Slider("Color Picker: ", defaultColour, 0, Colors.Count() - 1));
             a.DisplayName = "Color Picker: " + ColorsName[a.CurrentValue];
-            a.OnValueChange += delegate { a.DisplayName = "Colour Picker: " + ColorsName[a.CurrentValue]; };
+            a.OnValueChange += delegate 
+            {
+                var t = 2000 + Utility.TickCount;
+                a.DisplayName = "Colour Picker: " + ColorsName[a.CurrentValue];
+                var color = menu.GetColor(uniqueId).ConvertColor();
+                Drawing.OnEndScene += delegate
+                {
+                    if (t >= Utility.TickCount)
+                    {
+                        Drawing.DrawLine(new Vector2(200, 195), new Vector2(200, 305), 110, System.Drawing.Color.Black);
+                        Drawing.DrawLine(new Vector2(200, 200), new Vector2(200, 300), 100, color);
+                        TestFont.DrawText(null, "Kappa123", 115, 320, menu.GetColor(uniqueId));
+                    }
+                };
+            };            
         }
 
         public static Color GetColor(this Menu m, string id)
@@ -73,6 +74,68 @@ namespace TimerBuddy
                 return Colors[number.CurrentValue];
             }
             return Color.White;
+        }
+
+
+        private static readonly DrawType[] DrawTypeList =
+        {
+            DrawType.Default, DrawType.HPLine, DrawType.Number, DrawType.NumberLine
+        };
+
+        private static readonly string[] DrawTypeName =
+        {
+            "Default", "Under HP bar", "Counter at position", "Counter and Bar at position"
+        };
+
+        public static void AddDrawTypeItem(this Menu menu, string uniqueId, int defaultLevel = 0)
+        {
+            var a = menu.Add(uniqueId, new Slider("Drawing Style: ", defaultLevel, 0, DrawTypeList.Count() - 1));
+            a.DisplayName = "Drawing Style: " + DrawTypeName[a.CurrentValue];
+            a.OnValueChange += delegate
+            {
+                a.DisplayName = "Drawing Style: " + DrawTypeName[a.CurrentValue];
+            };
+        }
+
+        public static DrawType GetDrawType(this Menu m, string id)
+        {
+            var number = m[id].Cast<Slider>();
+            if (number != null)
+            {
+                return DrawTypeList[number.CurrentValue];
+            }
+            return DrawType.Default;
+        }
+
+
+        private static readonly Importance[] ImportanceList =
+        {
+            Importance.Low, Importance.Medium, Importance.High
+        };
+
+        private static readonly string[] ImportanceName =
+        {
+            "Low", "Medium", "High"
+        };
+
+        public static void AddImportanceItem(this Menu menu, string uniqueId, int defaultLevel = 1)
+        {
+            var a = menu.Add(uniqueId, new Slider("Importance Level: ", defaultLevel, 0, ImportanceList.Count() - 1));
+            a.DisplayName = "Importance Level: " + ImportanceName[a.CurrentValue];
+            a.OnValueChange += delegate
+            {
+                a.DisplayName = "Importance Level: " + ImportanceName[a.CurrentValue];
+            };
+        }
+
+        public static Importance GetImportance(this Menu m, string id)
+        {
+            var number = m[id].Cast<Slider>();
+            if (number != null)
+            {
+                return ImportanceList[number.CurrentValue];
+            }
+            return Importance.Medium;
         }
     }
 }
