78414952da46b6226bc67e69d9f267dfedaecb5e
SuperNova911 <suwhan77@naver.com>
2016-01-30 23:42:32 +0900

    Add FindCasterWard

--- TimerBuddy/TimerBuddy/Utility.cs
+++ TimerBuddy/TimerBuddy/Utility.cs
@@ -1,5 +1,9 @@
 ﻿using EloBuddy;
 using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -157,13 +161,13 @@ namespace TimerBuddy
                 switch (spell.SpellType)
                 {
                     case SpellType.SummonerSpell:
-                        return Config.SummonerMenu.GetColor(spell.MenuString + "color");
+                        return Config.SummonerMenu.GetColor(spell.MenuCode + "color");
                     case SpellType.Spell:
-                        return Config.SpellMenu.GetColor(spell.MenuString + "color");
+                        return Config.SpellMenu.GetColor(spell.MenuCode + "color");
                     case SpellType.Trap:
-                        return Config.TrapMenu.GetColor(spell.MenuString + "color");
+                        return Config.TrapMenu.GetColor(spell.MenuCode + "color");
                     case SpellType.Item:
-                        return Config.ItemMenu.GetColor(spell.MenuString + "color");
+                        return Config.ItemMenu.GetColor(spell.MenuCode + "color");
                 }
                 return SharpDX.Color.White;
             }
@@ -175,7 +179,7 @@ namespace TimerBuddy
             }
         }
 
-        public static System.Drawing.Color ConvertColor(SharpDX.Color color)
+        public static System.Drawing.Color ConvertColor(this SharpDX.Color color)
         {
             return System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
         }
@@ -243,6 +247,66 @@ namespace TimerBuddy
             }
         }
 
+        public static Obj_AI_Base FindCasterWard(this GameObject sender, Spell database)
+        {
+            try
+            {
+                SpellCaster caster;
+                switch (database.ObjectName)
+                {
+                    case "YellowTrinket":
+                        caster = Program.CasterList.FirstOrDefault(d => d.Name == "TrinketTotemLvl1");
+
+                        if (caster != null)
+                            return caster.Caster;
+                        break;
+                    case "SightWard":
+                        caster = Program.CasterList.FirstOrDefault(d => d.Name == "ItemGhostWard");
+
+                        if (caster != null)
+                            return caster.Caster;
+                        break;
+                    case "BlueTrinket":
+                        caster = Program.CasterList.FirstOrDefault(d => d.Name == "TrinketOrbLvl3");
+
+                        if (caster != null)
+                            return caster.Caster;
+                        break;
+                    case "VisionWard":
+                        caster = Program.CasterList.FirstOrDefault(d => d.Name == "VisionWard");
+
+                        if (caster != null)
+                            return caster.Caster;
+                        break;
+                }
+
+                if (sender.Team.IsAlly())
+                {
+                    var hero = EntityManager.Heroes.Allies.OrderBy(d => d.Distance(sender.Position)).First();
+
+                    if (hero != null)
+                        return hero;
+                }
+
+                if (sender.Team.IsEnemy())
+                {
+                    var hero = EntityManager.Heroes.Enemies.OrderBy(d => d.Distance(sender.Position)).First();
+
+                    if (hero != null)
+                        return hero;
+                }
+
+                Chat.Print("<font color='#FF0000'>ERROR:</font> CODE FIND_CASTER_WARD2 " + sender.Name, Color.LightBlue);
+                return Player.Instance;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Chat.Print("<font color='#FF0000'>ERROR:</font> CODE FIND_CASTER_WARD " + sender.Name, Color.LightBlue);
+                return Player.Instance;
+            }
+        }
+
         public static string BaseObjectName(this GameObject sender)
         {
             var baseObject = sender as Obj_AI_Base;
@@ -284,6 +348,130 @@ namespace TimerBuddy
             if (unit.IsMinion || unit.IsMonster || unit.IsWard())
                 return false;
             return true;
+        }
+
+        public static Vector3 KappaRoss(this Spell spell)
+        {
+            Vector3 Kappa;
+            Vector3 vec3 = spell.CastPosition - spell.StartPosition;
+            var length = Math.Sqrt(Math.Pow(vec3.X, 2) + Math.Pow(vec3.Y, 2));
+
+            if (length > spell.Range)
+            {
+                Kappa.X = vec3.X / (float)length;
+                Kappa.Y = vec3.Y / (float)length;
+                Kappa.Z = vec3.Z / (float)length;
+
+                return Kappa * spell.Range + spell.StartPosition;
+            }
+
+            return spell.CastPosition;
+        }
+
+        public static void CloneTracker()
+        {
+            foreach (var enemy in EntityManager.Heroes.AllHeroes.Where(d => !d.IsDead && d.VisibleOnScreen &&
+            (d.BaseSkinName == "Leblanc" || d.BaseSkinName == "Shaco" || d.BaseSkinName == "MonkeyKing" || d.BaseSkinName == "Yorick")))
+            {
+                new Circle { Color = System.Drawing.Color.Gold, Radius = enemy.BoundingRadius, BorderWidth = 2 }.Draw(enemy.Position);
+            }
+        }
+
+        public static void AddCheckBox(this Menu menu, string uid, string displayName, bool defaultvalue)
+        {
+            try
+            {
+                menu.Add(uid, new CheckBox(displayName, defaultvalue));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Chat.Print("<font color='#FF0000'>ERROR:</font> CODE ADD_CHECKBOX " + displayName, Color.LightBlue);
+            }
+        }
+
+        public static void AddSlider(this Menu menu, string uid, string displayName, int a, int b, int c)
+        {
+            try
+            {
+                menu.Add(uid, new Slider(displayName, a, b, c));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Chat.Print("<font color='#FF0000'>ERROR:</font> CODE ADD_SLIDER " + displayName, Color.LightBlue);
+            }
+        }
+
+        public static int ToInt(this DrawType type)
+        {
+            try
+            {
+                switch (type)
+                {
+                    case DrawType.Default:
+                        return 0;
+                    case DrawType.HPLine:
+                        return 1;
+                    case DrawType.Number:
+                        return 2;
+                    case DrawType.NumberLine:
+                        return 3;
+                }
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Chat.Print("<font color='#FF0000'>ERROR:</font> CODE DRAW_TYPE_TO_INT " + type.ToString(), Color.LightBlue);
+                return 0;
+            }
+        }
+
+        public static int ToInt(this Importance type)
+        {
+            try
+            {
+                switch (type)
+                {
+                    case Importance.Low:
+                        return 0;
+                    case Importance.Medium:
+                        return 1;
+                    case Importance.High:
+                        return 2;
+                }
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Chat.Print("<font color='#FF0000'>ERROR:</font> CODE IMPORTANCE_TO_INT " + type.ToString(), Color.LightBlue);
+                return 0;
+            }
+        }
+
+        public static int ToInt(this Color color)
+        {
+            try
+            {
+                if (color == Color.HotPink)
+                    return 51;
+                if (color == Color.Yellow)
+                    return 17;
+                if (color == Color.LawnGreen)
+                    return 25;
+                if (color == Color.SkyBlue)
+                    return 33;
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Chat.Print("<font color='#FF0000'>ERROR:</font> CODE IMPORTANCE_TO_INT " + color.ToString(), Color.LightBlue);
+                return 0;
+            }
         }
     }
 }
