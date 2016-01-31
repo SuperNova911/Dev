a9d03bfa904aa79b319bd5d2195d927dc001b5f7
SuperNova911 <suwhan77@naver.com>
2016-02-01 00:02:08 +0900

    SC2 Icon add

--- TimerBuddy/TimerBuddy/TextureDraw.cs
+++ TimerBuddy/TimerBuddy/TextureDraw.cs
@@ -25,7 +25,7 @@ namespace TimerBuddy
 
         private static Sprite MainBar { get; set; }
 
-        private static readonly Dictionary<string, Sprite> SpriteList = new Dictionary<string, Sprite>();
+        public static readonly Dictionary<string, Sprite> SpriteList = new Dictionary<string, Sprite>();
 
         static TextureDraw()
         {
@@ -37,6 +37,7 @@ namespace TimerBuddy
                 var itemList = SpellDatabase.Database.Where(i => i.SpellType == SpellType.Item).ToList();
                 var trapList = SpellDatabase.Database.Where(t => heroName.Contains(t.ChampionName) && t.SpellType == SpellType.Trap).ToList();
                 var spellList = SpellDatabase.Database.Where(s => heroName.Contains(s.ChampionName) && s.SpellType == SpellType.Spell).ToList();
+                var sc2List = SC2TimerDatabase.Database.Where(d => (heroName.Contains(d.ChampionName) && d.SC2Type == SC2Type.Spell) || (d.SC2Type == SC2Type.SummonerSpell)).ToList();
 
                 foreach (var list in summonerList)
                 {
@@ -73,6 +74,30 @@ namespace TimerBuddy
                     TextureLoader.Load(list.MenuCode, list.SpriteName);
                     SpriteList.Add(list.MenuCode, new Sprite(() => TextureLoader[list.MenuCode]));
                 }
+
+                foreach (var list in sc2List)
+                {
+                    var menucode = list.SC2Type ==
+                        SC2Type.SummonerSpell ? "sc2" + list.DisplayName : 
+                        list.SC2Type == SC2Type.Spell ? "sc2" + list.ChampionName + list.Slot.ToString() : 
+                        list.DisplayName;
+
+                    if (SpriteList.ContainsKey(menucode))
+                        continue;
+
+                    TextureLoader.Load(menucode, list.SpriteName);
+                    SpriteList.Add(menucode, new Sprite(() => TextureLoader[menucode]));
+                }
+
+                if (true)
+                {
+                    TextureLoader.Load("SC2Blue", Resources.SC2Blue);
+                    SpriteList.Add("SC2Blue", new Sprite(() => TextureLoader["SC2Blue"]));
+                    TextureLoader.Load("SC2Orange", Resources.SC2Orange);
+                    SpriteList.Add("SC2Orange", new Sprite(() => TextureLoader["SC2Orange"]));
+                    TextureLoader.Load("SC2Red", Resources.SC2Red);
+                    SpriteList.Add("SC2Red", new Sprite(() => TextureLoader["SC2Red"]));
+                }
             }
             catch (Exception e)
             {
@@ -101,6 +126,22 @@ namespace TimerBuddy
             var sprite = SpriteList["Summoner Teleport"];
 
             sprite.Draw(vector2 + new Vector2(-18, 0));
+        }
+
+        public static void DrawSC2Hud(Team team, Vector2 position)
+        {
+            switch (team)
+            {
+                case Team.Ally:
+                    SpriteList["SC2Blue"].Draw(position);
+                    break;
+                case Team.Enemy:
+                    SpriteList["SC2Red"].Draw(position);
+                    break;
+                case Team.Neutral:
+                    SpriteList["SC2Orange"].Draw(position);
+                    break;
+            }
         }
         
         public static void Initialize()
