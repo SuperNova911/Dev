f4ceba4c6e753908fe536cc40bbbf9b5523cfd7e
SuperNova911 <suwhan77@naver.com>
2016-02-05 15:22:15 +0900

    New Color

--- TimerBuddy/TimerBuddy/Properties/Resources.Designer.cs
+++ TimerBuddy/TimerBuddy/Properties/Resources.Designer.cs
@@ -5693,6 +5693,16 @@ namespace TimerBuddy.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap SC2Green {
+            get {
+                object obj = ResourceManager.GetObject("SC2Green", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap SC2Orange {
             get {
                 object obj = ResourceManager.GetObject("SC2Orange", resourceCulture);
